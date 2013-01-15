ff860f4a519b884a7ac8b722f97a79270d530739
viktor <viktor@viktor-skelia.(none)>
2013-01-15 17:55:18 +0200

    3p libraries have been added.

---
+++ js/plug.js
@@ -0,0 +1,12 @@
+var plug = function() {
+	var _this = this;
+	_this.init = function() {
+
+	}
+	_this.init();
+}
+
+jQuery(document).ready(function() {
+	alert("document is ready.");
+	jQuery("body").removeClass("loading");
+});
